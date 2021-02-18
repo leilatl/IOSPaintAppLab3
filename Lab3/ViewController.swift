8559b3e56f343d57b9c099428903e973e8bb30ca
leila <leilatheleph@gmail.com>
2021-02-18 13:26:06 +0600

    Initial Commit

---
+++ Lab3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lab3
+//
+//  Created by Leila on 2/18/21.
+//  Copyright © 2021 Leila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
